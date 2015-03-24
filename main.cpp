a493f4aad6e2acbcb23984728e7dfd9952ca020f
Derek Hua <dhua20@yahoo.com>
2015-03-24 01:04:12 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NonFloatingPointCalculator
+//
+//  Created by Derek Hua on 3/24/15.
+//  Copyright (c) 2015 Derek Hua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
